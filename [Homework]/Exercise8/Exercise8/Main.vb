8036133a3bc0f133c237a77acb4d737c1cf71e3d
Victor Velasco <victor.velasco@student.swic.edu>
2019-10-29 15:20:40 -0500

    Oct 29 Update

--- [Homework]/Exercise8/Exercise8/Main.vb
+++ [Homework]/Exercise8/Exercise8/Main.vb
@@ -4,7 +4,7 @@
         New SalariedEmployee("John", "Smith", "111-11-1111", 800.0),
         New CommissionEmployee("Sue", "Jones", "222-22-2222", 10000.0, 0.06),
         New BasePlusCommissionEmployee("Bob", "Lewis", "333-33-3333", 5000.0, 0.04, 300.0),
-        New PieceWorker("Gob", "Bluth", "444-44-4444", 69.0, 100),
+        New PieceWorker("Gob", "Bluth", "444-44-4444", 100.0, 10),
         New HourlyWorker("Michael", "Bluth", "555-55-5555", 48.0, 15.0)
     }
     Sub Main()
@@ -16,7 +16,7 @@
 
             ' Console color
             If counter Mod 2 = 0 Then
-                Console.ForegroundColor = ConsoleColor.DarkGray
+                Console.ForegroundColor = ConsoleColor.Green
             Else
                 Console.ForegroundColor = ConsoleColor.Cyan
             End If
