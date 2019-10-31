ef78323c3fd1bf56cb5b517e5c8bdd4ad9fa72a3
Victor Velasco <victor.velasco@student.swic.edu>
2019-10-30 19:26:47 -0500

    Created Circle.vb

---
+++ [Homework]/Circle.vb
@@ -0,0 +1,27 @@
+Public Class Circle
+
+Private _side As Double
+
+' Constructor
+Public New(s As Double)
+Side = s
+End New
+
+Public Property Side As Double
+Get
+Return _side
+End Get
+Set (value as Double) ' Validate that the side is not negative
+If value > 0 Then
+_side = value
+Else
+Throw New ArgumentOutOfRangeException("Side value need to be greater than zero.")
+End If
+End Set
+End Property
+
+Public Function GetArea()
+Return Side * Side
+End Function
+
+End Class
